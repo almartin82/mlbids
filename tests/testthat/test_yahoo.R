2d96a5094ade9185f486675934763ccae4ca4c5b
almartin82 <almartin@gmail.com>
2016-02-09 22:30:56 -0500

    more mlbids

--- tests/testthat/test_yahoo.R
+++ tests/testthat/test_yahoo.R
@@ -8,11 +8,3 @@ test_that("yahoo response returns valid player data", {
   expect_equal(y$name, 'Mike Trout')
 })
 
-
-test_that("yahoo response returns valid player data", {
-
-  y <- yahoo(11)
-
-  expect_is(y, 'data.frame')
-  expect_equal(y$name, 'Mike Trout')
-})
