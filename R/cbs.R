9e2725348aec4c89895715e4153de5f6fec861ed
almartin82 <almartin@gmail.com>
2016-02-15 17:01:57 -0500

    method for individual cbs players

--- R/cbs.R
+++ R/cbs.R
@@ -1,7 +1,74 @@
+#' CBS scrape
+#'
+#' @description gets the name, dob, etc for a player, given
+#' a cbsid value (an integer, generally between 1263000 and 2218000)
+#' @param id an integer
+#'
+#' @return if given a valid id, a data frame with player name, dob, id, etc.
+#' @export
 
-cbs <- function() {
-  stop('no need to scrape cbs player ids one by one.  see `cbs_bulk()`.')
+cbs <- function(id) {
+  url <- paste0('http://www.cbssports.com/mlb/players/playerpage/', id)
+  resp <- httr::GET(url)
+  if (!resp$status_code == 200) {
+    stop('Bad response from cbssports.com')
+  }
+
+  h <- read_html(resp$content)
+  cbs_title <- h %>% html_nodes('title') %>% html_text()
+  if (cbs_title == ' , , , MLB Baseball - CBSSports.com ') {
+    stop('No such playerid at cbssports.com')
+  }
+
+  demog_xml <- h %>%
+    html_nodes("#gridContainer .featureComponent") %>%
+    extract(1)
+
+  demog_keys <- demog_xml %>%
+    html_nodes('dt') %>%
+    xml_text() %>%
+    as.list() %>%
+    unlist()
+
+  demog_values <- demog_xml %>%
+    html_nodes('dd') %>%
+    xml_text() %>%
+    as.list() %>%
+    unlist()
+
+  demog_df <- data.frame(
+    keys = demog_keys,
+    values = demog_values,
+    stringsAsFactors = FALSE
+  )
+
+  fullname <- cbs_title %>%
+    strsplit(split = ',')  %>%
+    extract2(1) %>% extract(1)
+
+  pos <- h %>%
+    html_nodes(".playerPosition") %>%
+    extract(1) %>%
+    html_text()
+
+  player <- data.frame(
+    'cbsid' = id,
+    'name' = fullname,
+    'position' = pos,
+    'dob' = demog_df[demog_df$keys == 'Birthdate:', ]$values %>% mdy(),
+    'hometown' = demog_df[demog_df$keys == 'Hometown:', ]$values,
+    stringsAsFactors = FALSE
+  )
+
+  player
 }
+
+
+#' Bulk CBS player download
+#'
+#' @return a data frame with all of the MLB players returned by the `/fantasy/player`
+#' CBS API endpoint
+#' @export
 
 cbs_bulk <- function() {
 
@@ -16,6 +83,3 @@ cbs_bulk <- function() {
 
   list2df(cbs_json$body$players)
 }
-
-
-
--- tests/testthat/test_cbs.R
+++ tests/testthat/test_cbs.R
@@ -5,3 +5,19 @@ test_that("cbs bulk gets current players", {
   r <- cbs_bulk()
   expect_equal(class(r), 'data.frame')
 })
+
+
+test_that("cbs scrape returns one player correctly", {
+
+  p <- cbs(1894641)
+  expect_equal(class(p), 'data.frame')
+
+  p <- cbs(1894642)
+  expect_equal(class(p), 'data.frame')
+
+})
+
+
+test_that("cbs scrape errors on known bad player", {
+  expect_error(cbs(1894653))
+})
